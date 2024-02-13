33663fd1b3521ca488a8945326f3aeb08359aa5b
dana <aidana1529@gmail.com>
2024-02-13 14:58:26 +0900

    FIX: ec2 port

--- src/config.tsx
+++ src/config.tsx
@@ -1,3 +1,3 @@
-const EC2 = "http://ec2-54-180-100-100.ap-northeast-2.compute.amazonaws.com:8000"
+const EC2 = "http://ec2-15-164-218-184.ap-northeast-2.compute.amazonaws.com:8000"
 
 export default EC2
